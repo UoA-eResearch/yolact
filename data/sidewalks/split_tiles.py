b2e7342939eb559eed4bd40207c817e6fc7585e5
Nick Young <nick.young@auckland.ac.nz>
2022-08-22 12:26:08 +1200

    scale up training data

--- data/sidewalks/split_tiles.py
+++ data/sidewalks/split_tiles.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python3.7
 
 import solaris as sol
+from glob import glob
+from tqdm.auto import tqdm
+import random
 
-train_test = {
-    "train": "input/BA32_3703.tif",
-    "valid": "input/BA32_3603.tif"
-}
+files = glob("input/*.tif")
+print(f"Found {len(files)} files")
 
-for k, v in train_test.items():
-    raster_tiler = sol.tile.raster_tile.RasterTiler(
-        dest_dir=k,  # the directory to save images to
-        src_tile_size=(700, 700),  # the size of the output chips
-        verbose=True,
-    )
-    raster_bounds_crs = raster_tiler.tile(v)
-    print(raster_bounds_crs)
+# Randomly choose 20% of tiles for validation
+random.seed(1)
+valid = random.sample(files, int(len(files) * 0.2))
+print(f"validation ({len(valid)}/{len(files)}): {valid}")
 
-    vector_tiler = sol.tile.vector_tile.VectorTiler(dest_dir=f"{k}_labels", verbose=True)
-    vector_tiler.tile(
-        "input/AKL_sidewalks.gpkg",
-        tile_bounds=raster_tiler.tile_bounds,
-        tile_bounds_crs=raster_bounds_crs,
-    )
+for f in tqdm(files):
+    if f in valid:
+        name = "valid"
+    else:
+        name = "train"
+    try:
+        raster_tiler = sol.tile.raster_tile.RasterTiler(
+            dest_dir=name,  # the directory to save images to
+            src_tile_size=(700, 700),  # the size of the output chips
+            verbose=False,
+        )
+        raster_bounds_crs = raster_tiler.tile(f)
+        vector_tiler = sol.tile.vector_tile.VectorTiler(
+            dest_dir=f"{name}_labels", verbose=True
+        )
+        vector_tiler.tile(
+            "input/AKL_sidewalks.gpkg",
+            tile_bounds=raster_tiler.tile_bounds,
+            tile_bounds_crs=raster_bounds_crs,
+        )
 
+    except Exception as e:
+        print(f"{e} for {f}, skipping")
+
+for name in ["train", "valid"]:
     coco_dict = sol.data.coco.geojson2coco(
-        k,
-        f"{k}_labels",
-        output_path=f"{k}_coco.json",
+        name,
+        f"{name}_labels",
+        output_path=f"{name}_coco.json",
         matching_re=r"(\d{7}_\d{7})",
-        verbose=True,
         explode_all_multipolygons=True,
+        verbose=False,
     )
